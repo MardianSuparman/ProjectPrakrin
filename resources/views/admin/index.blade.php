94fb1ae258e36e139998b03196b4839d3fd3d743
MardianSuparman <tugasonline05@gmail.com>
2024-06-22 12:36:40 +0700

    prakerin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.admin')
+@section('content')
+<h1>ini Halaman admin</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,34 @@
 <?php
 
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 // root menegecek admin
-Route::get('test', function(){
-    return view('layouts.admin');
+// Route::get('test', function(){
+//     return view('layouts.front');
+// });
+
+// route admin (backend)
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('/', function(){
+        return view('admin.index');
+    });
+    // untuk route admin lainnya
 });
+
+//rote frotend
+Route::get('/',[FrontController::class, 'index']);
+Route::get('contact',[FrontController::class, 'contact']);
+Route::get('shop',[FrontController::class, 'shop']);
+Route::get('cart',[FrontController::class, 'cart']);
+Route::get('checkout',[FrontController::class, 'checkout']);
+Route::get('track',[FrontController::class, 'track']);
+
